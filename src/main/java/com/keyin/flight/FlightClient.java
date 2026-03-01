f90c231f9fcd56499a07c03562d03166ae59c76c
BigSponce <BigSponce@gmail.com>
2026-03-01 15:15:00 -0330

    Added basic contents to+* FlightClient.java

--- src/main/java/com/keyin/flight/FlightClient.java
+++ src/main/java/com/keyin/flight/FlightClient.java
@@ -1,4 +1,36 @@
 package com.keyin.flight;
 
-public class FlightClient {
+import com.keyin.common.RestClient;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+public class flightClient {
+
+    private RestClient restClient;
+
+    public List<Flight> getAllFlights() {
+
+        if (getRestClient() != null) {
+
+            restClient.setURLRoot("http://localhost:8080/airport");
+
+            String response = restClient.sendGetRequest();
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            return mapper.readValue(response, new TypeReference<List<Flight>>() {});
+        }
+
+        return List.of();
+    }
+
+    public RestClient getRestClient() {
+        return restClient;
+    }
+
+    public void setRestClient(RestClient restClient) {
+        this.restClient = restClient;
+    }
 }
