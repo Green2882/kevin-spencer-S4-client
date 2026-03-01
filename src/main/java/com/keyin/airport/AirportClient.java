e45a0561f5861f37a80ee33eec6a0fc7e4283acd
Kevin Green <Greenkevin28@icloud.com>
2026-03-01 15:43:12 -0330

    Set up airportClient

--- src/main/java/com/keyin/airport/AirportClient.java
+++ src/main/java/com/keyin/airport/AirportClient.java
@@ -1,5 +1,37 @@
 package com.keyin.airport;
 
+import com.keyin.aircraft.Aircraft;
+import com.keyin.common.RestClient;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
 public class AirportClient {
 
+    private RestClient restClient;
+
+    public List<Airport> getAllAirports() {
+
+        if (getRestClient() != null) {
+
+            restClient.setURLRoot("http://localhost:8080/airports");
+
+            String response = restClient.sendGetRequest();
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            return mapper.readValue(response, new TypeReference<List<Airport>>() {});
+        }
+
+        return List.of();
+    }
+
+    public RestClient getRestClient() {
+        return restClient;
+    }
+
+    public void setRestClient(RestClient restClient) {
+        this.restClient = restClient;
+    }
 }
