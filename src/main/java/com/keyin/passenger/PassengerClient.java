ec4d044b501e92ddad54501f75934b47a5b679ad
BigSponce <BigSponce@gmail.com>
2026-03-01 15:16:51 -0330

    Added basic contents to PassengerClient.java

--- src/main/java/com/keyin/passenger/PassengerClient.java
+++ src/main/java/com/keyin/passenger/PassengerClient.java
@@ -1,4 +1,36 @@
 package com.keyin.passenger;
 
-public class PassengerClient {
+import com.keyin.common.RestClient;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+public class passengerClient {
+
+    private RestClient restClient;
+
+    public List<Passenger> getAllPassengers() {
+
+        if (getRestClient() != null) {
+
+            restClient.setURLRoot("http://localhost:8080/airport");
+
+            String response = restClient.sendGetRequest();
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            return mapper.readValue(response, new TypeReference<List<Passenger>>() {});
+        }
+
+        return List.of();
+    }
+
+    public RestClient getRestClient() {
+        return restClient;
+    }
+
+    public void setRestClient(RestClient restClient) {
+        this.restClient = restClient;
+    }
 }
