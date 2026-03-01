46405c7bfeec084b1669f970c5b3b31fa2bd3c62
Kevin Green <Greenkevin28@icloud.com>
2026-03-01 13:08:09 -0330

    Set up aircraft package files. Set up aircraft class

---
+++ src/main/java/com/keyin/aircraft/Aircraft.java
@@ -0,0 +1,55 @@
+package com.keyin.aircraft;
+
+public class Aircraft {
+    private Long id;
+    private String type;
+    private String airlineName;
+    private String numOfPassengers;
+
+    public Aircraft(String type, String airlineName, String numOfPassengers) {
+        this.type = type;
+        this.airlineName = airlineName;
+        this.numOfPassengers = numOfPassengers;
+    }
+
+    public Aircraft() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public String getNumOfPassengers() {
+        return numOfPassengers;
+    }
+
+    public void setNumOfPassengers(String numOfPassengers) {
+        this.numOfPassengers = numOfPassengers;
+    }
+
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Aircraft{" +
+                "id='" + id + '\'' +
+                ", type='" + type + '\'' +
+                ", airlineName='" + airlineName + '\'' +
+                ", numOfPassengers='" + numOfPassengers + '\'' +
+                '}';
+    }
+}
+
