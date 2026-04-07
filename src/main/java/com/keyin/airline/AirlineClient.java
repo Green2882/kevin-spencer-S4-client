0267ab6707b287e3f288cab392daf053d9976569
BigSponce <BigSponce@gmail.com>
2026-04-07 14:48:39 -0230

    Added Airline folder and Airline.java, AirlineClient.java files.

---
+++ src/main/java/com/keyin/airline/AirlineClient.java
@@ -0,0 +1,37 @@
+package com.keyin.airline;
+
+import com.keyin.aircraft.Aircraft;
+import com.keyin.common.RestClient;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+public class AirlineClient {
+
+    private RestClient restClient;
+
+    public List<Airline> getAllAirlines() {
+
+        if (getRestClient() != null) {
+
+            restClient.setURLRoot("http://localhost:8080/airlines");
+
+            String response = restClient.sendGetRequest();
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            return mapper.readValue(response, new TypeReference<List<Airline>>() {});
+        }
+
+        return List.of();
+    }
+
+    public RestClient getRestClient() {
+        return restClient;
+    }
+
+    public void setRestClient(RestClient restClient) {
+        this.restClient = restClient;
+    }
+}
