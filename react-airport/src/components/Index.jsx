c91a1c7e791b93d4df54e7c5977462350e34f8a8
BigSponce <BigSponce@gmail.com>
2026-04-10 15:09:52 -0230

    Made font size consistent across links in header, added placeholder options for selecting airports

--- react-airport/src/components/Index.jsx
+++ react-airport/src/components/Index.jsx
@@ -12,7 +12,7 @@ const Home = () => {
             <h1 className="header-title">Airport & Flight Tracker</h1>
           </Link>
           <div className="login-link">
-            <Link to="/login">Log in</Link>
+            <Link to="/login">Log out</Link>
           </div>
           <div className="flight-link">
             <Link to="/flights">Flights</Link>
@@ -25,6 +25,12 @@ const Home = () => {
                 <option value="2">2</option>
                 <option value="3">3</option>
                 <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
               </select>
               <input type="submit" value="View" />
             </form>
