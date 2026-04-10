27a3c7b311728e845965950216952944e11ea605
BigSponce <BigSponce@gmail.com>
2026-04-10 13:09:48 -0230

    Set up more styles for App.css, Set up basic routing in App.jsx, Created links for Flight and Login in Index.jsx

--- react-airport/src/App.jsx
+++ react-airport/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import "./App.css";
+import Index from "./components/Index.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/index" element={<Index />} />
     </Routes>
   );
 }
--- react-airport/src/components/Index.jsx
+++ react-airport/src/components/Index.jsx
@@ -1,43 +1,33 @@
-import {useNavigate, Link} from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-const Home= () => {
-    const navigate = useNavigate();
+const Home = () => {
+  const navigate = useNavigate();
 
-    return (
-        <>
-            <div className="index-box">
-                <div className="header-links">
-                    <Link to="/" style={{
-                        color: "black",
-                        textDecoration: "none",
-                        float: "left"
-                    }}>
-                        <h1 className="header-title">
-                            Airport & Flight Tracker
-                        </h1>
-                    </Link>
-                    <Link to="/login" style={{
-                        color: "black",
-                        textDecoration: "none",
-                        float: "right",
-                    }}>
-                        <p>
-                            Log in
-                        </p>
-                    </Link>
-                    <Link to="/flights" style={{
-                        color: "black",
-                        textDecoration: "none",
-                        float: "right",
-                    }}>
-                        <p>
-                            Flights
-                        </p>
-                    </Link>
-
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="index-box">
+        <div className="links-box">
+          <Link
+            to="/"
+            style={{
+              color: "black",
+              textDecoration: "none",
+              float: "left",
+            }}
+          >
+            <h1 className="header-title">Airport & Flight Tracker</h1>
+          </Link>
+          <div className="login-link">
+            <Link to="/login">Log in</Link>
+          </div>
+          <div className="flight-link">
+            <Link to="/flights">Flights</Link>
+          </div>
+        </div>
+        <div className="content-box"></div>
+      </div>
+    </>
+  );
+};
+export default Home;
